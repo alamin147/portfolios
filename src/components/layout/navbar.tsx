2a2c25de434c5e04192226ec7e7e007fd7fdf9cc
alamin147 <alamin.14780@gmail.com>
2025-11-01 18:42:03 +0600

    feat: remove unused DiLinux icon import from navbar component

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import { DiLinux } from "react-icons/di";
 import { Link } from "react-router-dom";
 import { CommonTooltip } from "@/components/shared";
 
