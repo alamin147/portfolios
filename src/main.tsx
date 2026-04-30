06ad59b5e0da67da689d4a4a68a43da1960e9888
alamin147 <alamin.14780@gmail.com>
2026-04-30 08:50:38 +0600

    feat: update toast notification theme to colored for better visibility

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')!).render(
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="dark"
+        theme="colored"
         toastClassName="custom-toast"
       />
     </HelmetProvider>
